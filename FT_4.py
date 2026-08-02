2aeac0231438f2de16436c27fed3a2191d016958
Ritesh Prask <riteshprask@gmail.com>
2026-08-02 22:00:55 +0530

    Update date range in FT_4.py

---
+++ FT_4.py
@@ -0,0 +1,27 @@
+# Historical Prices - OHLCV
+
+from fyers_apiv3 import fyersModel
+import json
+
+client_id = "XC4XXXXM-100" # obtained by sample code in Fyers API documentation, no need to change in each run
+access_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOlsiZDoxIiwiZDoyIiwieDowIiwieDoxIl0sImF0X2hhc2giOiJnQUFBQUFCcWFGWTVJZnlzY3BiLTRvY3RaUTBnNF84NmktUmUtdEhTb1FUaXNtR1c3di1XMExlSU1Tcnh4OVJ5THdTdmtxblFoY0pFNzdZTU1wTFJ0ZVpOZ0pOLW16N0VoNGs5bEdVdWZmRjBpdHR4blFkUE9QTT0iLCJkaXNwbGF5X25hbWUiOiIiLCJvbXMiOiJLMSIsImhzbV9rZXkiOiJkMDY4YzRmZDQzYzVkNDZkMzVhYWQ2NmY2ZGMzYThjNTA2ZDBkNDY2NzI2YmVmYjAwYzdkMTliYSIsImlzRGRwaUVuYWJsZWQiOiJOIiwiaXNNdGZFbmFibGVkIjoiTiIsImZ5X2lkIjoiWVAwNjM4MCIsImFwcFR5cGUiOjEwMCwiZXhwIjoxNzg1Mjg1MDAwLCJpYXQiOjE3ODUyMjI3MTMsImlzcyI6ImFwaS5meWVycy5pbiIsIm5iZiI6MTc4NTIyMjcxMywic3ViIjoiYWNjZXNzX3Rva2VuIn0.Cse5WS-um7M9wA7qO7wbh4x2Y7yiT2GZLQ-ZQI6r_vA"
+
+# Initialize the FyersModel instance with your client_id, access_token, and enable async mode
+fyers = fyersModel.FyersModel(client_id=client_id, is_async=False, token=access_token, log_path="")
+
+# https://myapi.fyers.in/docsv3#tag/Data-Api/paths/~1DataApi/post for data parameters
+data = {
+    "symbol":"NSE:OLAELEC-EQ",
+    "resolution":"60",
+    "date_format":"1",
+    "range_from":"2026-07-01",
+    "range_to":"2026-07-31",
+    "cont_flag":"1"
+}
+
+response = fyers.history(data=data)
+# print(response)
+
+pretty_json = json.dumps(response, indent=4)
+print(pretty_json)
+
